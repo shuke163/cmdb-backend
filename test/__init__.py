e7f69eae43f266d8395e74c00d9ac816cb323b63
shu_ke <shu_ke163@163.com>
2024-12-31 10:18:01 +0800

    first commit

---
+++ test/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   __init__.py.py    
+@Contact :   shu_ke163@163.com
+@Author  :   FengFeng zhao
+@Modify Time      @Version    @Desciption
+------------      --------    -----------
+2024/12/31 09:38    1.0         None
+"""
